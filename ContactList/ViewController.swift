44766547367d0087dd575de31d02adb7ba3df8a5
Alex Zhang <zl22222222@hotmail.com>
2015-05-06 14:40:21 +1200

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Zhang Le on 15/5/6.
+//  Copyright (c) 2015年 LeZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
